402f6b5e70f64503f8153cafe460129764a8d86c
unknown <ricardogusar93@hotmail.com>
2021-09-30 14:02:05 -0500

    ejercicios semana 4

--- semana4/challenge1.js
+++ semana4/challenge1.js
@@ -1,6 +1,12 @@
 function keysAndValues(objInput) {
     //Implementación
-    return 0;
+    const keys = Object.keys(objInput);
+    const entry = Object.entries(objInput);
+
+    const sortKeys = keys.sort((a, b) => a -b);
+    const valuesArray = entry.map(item => item[1]);
+
+    return [valuesArray]
 }
 
 module.exports = keysAndValues;
--- semana4/challenge2.js
+++ semana4/challenge2.js
@@ -1,6 +1,17 @@
 function isEqual(objInput1, objInput2) {
     //Implementación
-    return 0;
+    const obj1 = Object.keys(objInput1);
+    const obj2 = Object.keys(objInput2);
+
+    if(obj1.length !== obj2.length) {
+        return false;
+    }
+    for(let i of obj1) {
+        if(objInput1[i] !== objInput2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = isEqual;
